f6e93a06a2f8d0aa43987774508ba351fe6bc9a1
shitikovkirill <sh.kiruh@gmail.com>
2017-04-10 10:19:57 +0300

    add STATIC_ROOT

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -128,11 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    '/path/to/yourenv/blogproject/static',
+    os.path.join(BASE_DIR, "blog/static"),
 )
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/path/to/yourenv/blogproject/media'
