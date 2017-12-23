79d25457958bb8a6d2857e0acd57cdc3d3b3c163
Shitikov Kyrylo <kshy@isddesign.com>
2017-12-24 00:04:34 +0200

    Fix bug with static files

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -127,12 +127,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATICFILES_DIRS = (
-    # os.path.join(BASE_DIR, "static/"),
+    os.path.join(BASE_DIR, "static/"),
     os.path.join(BASE_DIR, "blog/static"),
 )
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
